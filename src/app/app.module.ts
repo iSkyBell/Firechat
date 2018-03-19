4f5d2b1747238df9865340b327dfeb20e6659248
iSkyBell <hernan.pk92@hotmail.com>
2018-03-19 10:29:04 -0500

    Observable con firebase
    Usar el observable para cambiar traer la data en tiempo real

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// imports de Firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 
+// pages
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase), // imports firebase/app needed for everything
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule // imports firebase/auth, only needed for auth features,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA5cSDq512FT-fZid1ZeauD4w_PaaHobW4",
+    authDomain: "firechat-d0d3f.firebaseapp.com",
+    databaseURL: "https://firechat-d0d3f.firebaseio.com",
+    projectId: "firechat-d0d3f",
+    storageBucket: "firechat-d0d3f.appspot.com",
+    messagingSenderId: "600385472842"
+  }
 };

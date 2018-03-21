11db7ad6d0cbc6e1176e9c068cae9af339616c8a
iSkyBell <hernan.pk92@hotmail.com>
2018-03-20 22:19:51 -0500

    Login con google
    Se hace el login con google

--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -2,14 +2,26 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
 import { Mensaje } from '../interfaces/mensaje.interface';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Injectable()
 export class ChatService {
 
   private itemsCollection: AngularFirestoreCollection<Mensaje>;
   public chats:Mensaje[] = [];
+  public usuario:any = {};
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore, public afAuth: AngularFireAuth) {
+    this.afAuth.authState.subscribe( user =>{
+      if( !user ){
+        return
+      }
+      this.usuario.nombre = user.displayName;
+      this.usuario.uid = user.uid;
+      console.log(this.usuario);
+    });
+   }
 
   cargarMensajes(){
     this.itemsCollection = this.afs.collection<Mensaje>('chats', 
@@ -28,5 +40,12 @@ export class ChatService {
     }
     return this.itemsCollection.add(mensajeDemo);
   }// fin agregarMensaje
+  
+  login(proveedor) {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }// fin login
+  logout() {
+    this.afAuth.auth.signOut();
+  }// fin logout
 
 }
